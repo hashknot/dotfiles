e5ffdbd1449e66dc28ac135d415da3a861cb9a0c
Jitesh <msg.jitesh@gmail.com>
2014-09-01 01:39:36 +0530

    Added quora and 9gag js/css files

---
+++ .js/9gag.tv.js
@@ -0,0 +1,1 @@
+$(".navbar").css({"display":"none"})
--- .js/default.js
+++ .js/default.js
@@ -1,1 +1,13 @@
 console.log("Dotjs is working");
+
+addCSSRule = function(rules){
+    var sheet = (function() {
+        var styleTag = document.createElement("style");
+        var head = document.getElementsByTagName("head")[0];
+        head.appendChild(styleTag);
+
+        return styleTag.sheet ? styleTag.sheet : styleTag.styleSheet;
+    })();
+
+    sheet.insertRule(rules, 0)
+}
