ce3a8e2a2e86ff1a4da8364e96220eecb1a2b80c
Jitesh <msg.jitesh@gmail.com>
2014-12-11 14:12:06 +0530

    Configure edit-command-line behavior
    Update vimrc to avoid "lcd" when in
    edit-command-line mode.
    
    Add custom script for configuring
    edit-command-line mode.

---
+++ .zsh/my-edit-command-line.zsh
@@ -0,0 +1,23 @@
+# Edit the command line using your usual editor.
+
+# Original author: godlygeek(https://github.com/godlygeek)
+# github.com/godlygeek/zsh-files/.zfunctions/edit-command-line
+function my-edit-command-line(){
+    local tmpfile=${TMPPREFIX:-/tmp/zsh}ecl$$
+    print -R - "$PREBUFFER$BUFFER" >$tmpfile
+    exec </dev/tty
+    NO_LCD=true ${VISUAL:-${EDITOR:-vi}} $tmpfile
+    print -z - "$(<$tmpfile)"
+    command rm -f $tmpfile
+    zle send-break # Force reload from the buffer stack
+}
+
+# Map my function as a widget named "edit-command-line"
+zle -N edit-command-line my-edit-command-line
+
+# Emacs style:
+# Enable Ctrl-x-e to trigger edit-command-line widget
+bindkey '^x^e' edit-command-line
+
+# Vi style:
+# bindkey -M vicmd v edit-command-line
