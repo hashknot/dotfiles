8749dd096f09abcba0f91c1da8390a3ef8825f97
Jitesh Kamble <io@jite.sh>
2026-03-07 16:19:50 -0800

    Diff watcher

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,7 @@ require('options')
 require('plugins')
 require('keymaps')
 require('autocmds')
+require('custom.directory-watcher').setup({
+  path = vim.fn.getcwd(),
+  debounce = 100,
+})
--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,5 +1,30 @@
 local actions = require('diffview.actions')
 
+local is_git_ignored = function(filepath)
+  vim.fn.system('git check-ignore -q ' .. vim.fn.shellescape(filepath))
+  return vim.v.shell_error == 0
+end
+
+local update_left_pane = function()
+  pcall(function()
+    local view = require('diffview.lib').get_current_view()
+    if view then
+      view:update_files()
+    end
+  end)
+end
+
+require('custom.directory-watcher').registerOnChangeHandler('diffview', function(filepath)
+  local is_in_dot_git_dir = filepath:match '/%.git/' or filepath:match '^%.git/'
+  if is_in_dot_git_dir or not is_git_ignored(filepath) then
+    update_left_pane()
+  end
+end)
+
+vim.api.nvim_create_autocmd('FocusGained', {
+  callback = update_left_pane,
+})
+
 require('diffview').setup {
   default_args = {
     DiffviewOpen = { '--imply-local' },
